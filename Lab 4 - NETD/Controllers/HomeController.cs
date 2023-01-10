58ec2a28dd37a000c0fdc6a1ee5adcd4268c2c4d
Shivam Janda <shivam.janda@dcmail.ca>
2023-01-09 19:43:54 -0500

    final commit

---
+++ Lab 4 - NETD/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Name: Shivam Janda
+ * Date: November 21, 2022
+ * Description: Home controller and its actions
+ */
+using Lab_4___NETD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_4___NETD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // create a list of textbooks used view apparials 
+        public static List<textbook> textbooks = new List<textbook>();
+
+      
+        public IActionResult Index()
+        {
+            // returns index view
+            return View();
+        }
+
+     
+        /// <summary>
+        /// Differtiantes with the two methods. Gets the the page for appraise
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet] public IActionResult Appraise()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// whem the user presses the appraise button
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        [HttpPost] public IActionResult Appraise(textbook a)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                ViewData["TextbookTitle"] = "Textbook Title: " + a.title;
+                ViewData["Version"] = "Verison: " + a.version;
+                ViewData["AppraisedPrice"] = "Appraised at: $" + a.appraisedPrice(a.price, a.condition);
+                //ad the textbook to the list of textbooks
+                textbooks.Add(a);
+                // if the texbook is valid then show the view appraisal screen
+                return View("ViewApprasial", a);
+            }
+            else
+            {
+                // otherwise return the failure screen
+                return View("FailureScreen");
+            }
+
+       
+        }
+
+        /// <summary>
+        /// will call the viewapprasials page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ViewApprasials()
+        {
+            // view the list of textbook objects
+            ViewBag.textbooks = textbooks;
+            return View();
+        }
+       
+    }
+}
---
+++ Lab 4 - NETD/Models/textbook.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Name: Shivam Janda
+ * Date: November 21, 2022
+ * Description: textbook class and its properties
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_4___NETD.Models
+{
+    public class textbook
+    {
+        /// <summary>
+        /// Accessors and Mutators
+        /// </summary>
+        public string title { get; set; }
+        public int isbn { get; set; }
+        public float version { get; set; }
+        public float price { get; set; }
+        public string condition { get; set; }
+
+        /// <summary>
+        /// Deafult Constructor
+        /// </summary>
+        public textbook()
+        {
+
+        }
+
+        /// <summary>
+        /// Parameterized constructor
+        /// </summary>
+        public textbook(string title, int isbn, float version, float price, string condition)
+        {
+            this.title = title;
+            this.isbn = isbn;
+            this.version = version;
+            this.price = price;
+            this.condition = condition;
+        }
+
+        /// <summary>
+        /// Appraised price that checks what condition it is and generating a price for it 
+        /// </summary>
+        public double appraisedPrice(float price, string conditon)
+        {
+            if (condition == "New")
+            {
+                return Math.Round((price / 2), 2);
+            }
+
+            else if (condition == "Good")
+            {
+                return Math.Round((price / 3), 2);
+            }
+
+            else
+            {
+                return Math.Round((price / 4), 2);
+            }
+        }
+        /// <summary>
+        /// Method that returns a string of the textbook and its attributes
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Your textbook: " + this.title +
+                ", Version: " + this.version +
+                ", was appraised at: $" + appraisedPrice(this.price, this.condition);
+        }
+    }
+}
